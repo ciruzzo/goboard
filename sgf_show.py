066ecc86c67d7b996fc7ce08f2faa04fc3fbb36f
Yoshihiro Ciruzzo Tsuchiya <tsuchiya.yoshihiro@gmail.com>
2016-03-21 21:24:40 +0900

    changes

--- sgf_show.py
+++ sgf_show.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 	import matplotlib.pyplot as plt
 
 	if len(sys.argv) < 2:
-		print "usage: %s sgf_file" % sys.argv[0]
+		print "usage: %s sgf_file (limit)" % sys.argv[0]
 		exit(1)
 
 	move = go.sgfmove(sys.argv[1])
